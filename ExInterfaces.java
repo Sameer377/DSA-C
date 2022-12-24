b143d235ff184b1c55751e0566b32bb9059a70f7
Sameer Shaikh <101447093+Sameer377@users.noreply.github.com>
2022-12-24 11:44:50 +0530

    Update ExInterfaces.java

--- ExInterfaces.java
+++ ExInterfaces.java
@@ -2,6 +2,7 @@ package interfaces;
 
 interface Transportable {
     String FULE = "Petrol";
+
     public abstract int getMaxSpeed();
 
     static String isEarthFriendly(boolean s) {
@@ -27,6 +28,6 @@ public class ExInterfaces {
         System.out.println("Speed : " + transportable.getMaxSpeed());
         System.out.println("Ecofriendly : " + Transportable.isEarthFriendly(false));
         System.out.println("Costly : " + transportable.isCostly(true));
-        System.out.println("Fule : "+ Transportable.FULE);
+        System.out.println("Fule : " + Transportable.FULE);
     }
 }
