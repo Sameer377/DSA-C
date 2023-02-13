b7985f0ec6070c5fca8a5bcc908509796d366f7d
Sameer Shaikh <101447093+Sameer377@users.noreply.github.com>
2023-02-13 08:13:38 +0530

    Update Queue.c

--- Queue.c
+++ Queue.c
@@ -1,89 +1,57 @@
-#include <stdio.h>
-int Queue[5];
-int MAX = 4;
-int START = 0;
-int END = 0;
-void inQueue()
-{
-    if (END <= MAX)
-    {
-        printf("Enter element : ");
-        int element;
-        scanf("%d", &element);
-        Queue[END] = element;
-        END = END + 1;
-    }
-    else if (END > MAX)
-    {
-        printf("Queue Overflow...");
-    }
-}
-void deQueue()
-{
-    if (END == 0 || START == END)
-    {
-        printf("Queue is Empty...");
-    }
-    else if (START != END)
-    {
-        START = START + 1;
-        printf("Element Deleted ... ");
-    }
-}
-void show()
-{
-    if (END == 0 || START == END)
-    {
-        printf("Queue is Empty...");
-    }
-    else
-    {
-        for (int i = START; i < END; i++)
-        {
-            printf(" %d ", Queue[i]);
+#include<stdio.h>
+#include<stdlib.h>
+#define MAX 5
+int queue[MAX];
+int front=-1;
+int rear=-1;
+void inQueue(){
+    if(front<MAX-1){
+        if(rear==-1){
+            rear=rear+1;
         }
-        printf("\n");
+       printf("\nEnter Element to push : ");
+       int ele;
+       scanf("%d",&ele);
+       front=front+1;
+       queue[front]=ele; 
+    }else{
+        printf("\nQueue is full...");
     }
 }
-void recentDeleted()
-{
-    if (START == 0)
-    {
-        printf("no record found...");
-    }
-    else
-    {
-        printf("%d", Queue[START - 1]);
+
+void deQueue(){
+    if(rear==front){
+        printf("Queue is Empty...");
+    }else{
+        rear=rear+1;
     }
 }
-void main()
-{
-    int loop = 1;
-    while (loop == 1)
-    {
-        printf("\n1.Inqueue\n2.Dequeue\n3.Show\n4.Recent Deleted\n5.Exit\n");
-        printf("Enter Your Choice : ");
-        int choice;
-        scanf("%d", &choice);
-        switch (choice)
-        {
-        case 1:
-            inQueue();
-            break;
-        case 2:
-            break;
-        case 3:
-            show();
-            break;
-        case 4:
-            recentDeleted();
-            break;
-        case 5:
-            loop = 0;
-            break;
-        default:
-            printf("Invalid Choice ...");
-            break;
-        }
+
+void display(){
+    if(rear==front){
+        printf("Queue is Empty...");
+    }else{
+        for(int i=rear;i<=front;i++){
+        printf("%d",queue[i]);
     }
+    }
+    
+}
+
+void main(){
+while(1){
+    printf("\n1.Inqueue\n2.deQueue\n3.display\n4.exit\nChoice : ");
+    int ch;
+    scanf("%d",&ch);
+    switch(ch){
+        case 1 : inQueue();
+        break;
+        case 2 : deQueue();
+        break;
+        case 3 : display();
+        break;
+        case 4 : exit(0); break;
+        default : printf("Invalid..."); break;
+    }
+}
 }
