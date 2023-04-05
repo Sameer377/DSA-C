c10139908584de84370ca2e05fe5a612233cb8d7
Sameer Shaikh <sameershaikh01072004@gmail.com>
2023-04-05 23:26:08 +0530

    MergeSort file Updated

--- MergeSort2.c
+++ MergeSort2.c
@@ -1,59 +1,72 @@
-#include<stdio.h>
+#include <stdio.h>
 
-void merge(int arr[],int si,int mid,int ei){
+void merge(int arr[], int si, int mid, int ei)
+{
 
-    int merge[ei-si+1];
+    int merge[ei - si + 1];
 
-    int idx1=si;
-    int idx2=mid+1;
+    int idx1 = si;
+    int idx2 = mid + 1;
     int x = 0;
 
-    while(idx1<=mid && idx2<=ei){
-        if(arr[idx1]<=arr[idx2]){
-            merge[x]=arr[idx1];
-            x++;idx1++;
-        }else{
-            merge[x]=arr[idx2];
-            x++;idx2++;
+    while (idx1 <= mid && idx2 <= ei)
+    {
+        if (arr[idx1] <= arr[idx2])
+        {
+            merge[x] = arr[idx1];
+            x++;
+            idx1++;
+        }
+        else
+        {
+            merge[x] = arr[idx2];
+            x++;
+            idx2++;
         }
     }
 
-    while(idx1<=mid){
-         merge[x]=arr[idx1];
-            x++;idx1++;
+    while (idx1 <= mid)
+    {
+        merge[x] = arr[idx1];
+        x++;
+        idx1++;
     }
 
-    while(idx2<ei){
-         merge[x]=arr[idx2];
-            x++;idx2++;
+    while (idx2 < ei)
+    {
+        merge[x] = arr[idx2];
+        x++;
+        idx2++;
     }
 
-    for(int i=0,j=si;i<(sizeof(merge)/sizeof(merge[0]));i++,j++){
-        arr[j]=merge[i];
+    for (int i = 0, j = si; i < (sizeof(merge) / sizeof(merge[0])); i++, j++)
+    {
+        arr[j] = merge[i];
     }
-
 }
-void divide(int arr[],int si,int ei){
-    if(si>=ei){
+void divide(int arr[], int si, int ei)
+{
+    if (si >= ei)
+    {
         return;
     }
 
-    int mid = si+(ei-si)/2;
-   
+    int mid = si + (ei - si) / 2;
 
-    divide(arr,si,mid);
-    divide(arr,mid+1,ei);
+    divide(arr, si, mid);
+    divide(arr, mid + 1, ei);
 
-    merge(arr,si,mid,ei);
-
+    merge(arr, si, mid, ei);
 }
 
-void main(){
-    int arr[]={9,8,7,6,5,4,3,2,1};
+void main()
+{
+    int arr[] = {9, 8, 7, 6, 5, 4, 3, 2, 1};
 
-    divide(arr,0,8);
+    divide(arr, 0, 8);
     printf("\nArray : ");
-    for(int k=0;k<9;k++){
-        printf("%d ",arr[k]);
+    for (int k = 0; k < 9; k++)
+    {
+        printf("%d ", arr[k]);
     }
 }
